0ff2b58c575aeb959bd8627a846efb7e2f4dbd10
lan3945663 <lan3945663@gmail.com>
2021-12-20 20:07:20 +0300

    fix: fix toy api

--- src/common/game-constants.ts
+++ src/common/game-constants.ts
@@ -1,2 +1,3 @@
-export const toyUrl = '/src/assets/json/data.json';
+export const toyUrl = 'https://rolling-scopes-school.github.io/lan3945663-JSFE2021Q3/christmas-task-part-1/src/assets/json/data.json';
 export const favQuantity = 20;
+// https://rolling-scopes-school.github.io/lan3945663-JSFE2021Q3/momentum/src/helpers/quotes.json
