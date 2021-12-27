5f6a8a892db5fedc3d86513fbd4ba46981b1c39d
lan3945663 <lan3945663@gmail.com>
2021-12-27 18:30:27 +0300

    fix: fix path

--- src/common/game-constants.ts
+++ src/common/game-constants.ts
@@ -5,4 +5,4 @@ export const favQuantity = 20;
 export const DEFAULT_BG = '1';
 export const DEFAULT_TREE = '1';
 export const DEFAULT_TRANSFORMY = 3;
-export const URL_AUDIO = '/src/assets/audio/audio.mp3';
+export const URL_AUDIO = './src/assets/audio/audio.mp3';
